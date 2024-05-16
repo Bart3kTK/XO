7f0dd45d62d0f778a0d284533cc96d7e6fb7c377
Bart3kTK <bkamieniarz55@wp.pl>
2024-05-16 21:25:20 +0200

    start of database

--- src/main/java/com/game/xo/data/GameRepo.java
+++ src/main/java/com/game/xo/data/GameRepo.java
@@ -1,30 +1,30 @@
-// package com.game.xo.data;
+package com.game.xo.data;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 
-// import com.game.xo.interfaces.IGameReposytory;
-// import com.game.xo.model.Game;
+import com.game.xo.interfaces.IGameReposytory;
+import com.game.xo.model.Game;
 
-// public class GameRepo {
+public class GameRepo {
 
-//     @Autowired
-//     private static IGameReposytory gameReposytory;
+    @Autowired
+    private static IGameReposytory gameReposytory;
 
-//     private static GameRepo instance;
+    private static GameRepo instance;
 
-//     public static synchronized GameRepo getInstance() {
-//         if (instance == null) {
-//             instance = new GameRepo();
-//         }
-//         return instance;
-//     }
+    public static synchronized GameRepo getInstance() {
+        if (instance == null) {
+            instance = new GameRepo();
+        }
+        return instance;
+    }
 
-//     public Iterable<Game> getGames() {
-//         return gameReposytory.findAll();
-//     } 
+    public Iterable<Game> getGames() {
+        return gameReposytory.findAll();
+    } 
 
-//     public void saveGame(Game game) {
-//         gameReposytory.save(game);
-//     }   
+    public void saveGame(Game game) {
+        gameReposytory.save(game);
+    }   
 
-// }
+}
--- src/main/java/com/game/xo/service/GameService.java
+++ src/main/java/com/game/xo/service/GameService.java
@@ -1,27 +1,28 @@
-// package com.game.xo.service;
+package com.game.xo.service;
 
-// import java.util.UUID;
+import java.util.ArrayList;
+import java.util.UUID;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
-// import com.game.xo.model.Game;
-// import com.game.xo.model.GameStatus;
-// import com.game.xo.model.Player;
+import com.game.xo.model.Game;
+import com.game.xo.model.GameStatus;
+import com.game.xo.model.Player;
 
-// import lombok.AllArgsConstructor;
+import lombok.AllArgsConstructor;
 
-// @Service
-// @AllArgsConstructor
-// public class GameService {
+@Service
+@AllArgsConstructor
+public class GameService {
 
-//     public Game createGame(Player player1) {
-//         Game game = new Game();
-//         game.setGameBoard(new int[3][3]);
-//         game.setGameId(UUID.randomUUID().toString());
-//         game.setPlayer1(player1);
-//         game.setStatus(GameStatus.WAITING_FOR_PLAYER);
-//         return game;
+    public Game createGame(Player player1) {
+        Game game = new Game();
+        game.setGameBoard(new ArrayList<>());
+        game.setGameId(UUID.randomUUID().toString());
+        game.setPlayer1(player1);
+        game.setStatus(GameStatus.WAITING_FOR_PLAYER);
+        return game;
 
-//     }   
+    }   
     
-// }
+}
